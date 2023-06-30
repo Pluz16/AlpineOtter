7cc86a636c6da1210a32d4002741030f830ef191
Francesco Punta <puntafrancesco@gmail.com>
2023-06-30 18:14:03 +0200

    Inizio CRU, dogController e Web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rotte per la gestione dei cani
+    Route::resource('dogs', DogController::class);
 });
 
 require __DIR__.'/auth.php';
